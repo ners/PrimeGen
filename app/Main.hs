17f4b7b2d6d82ea2215872b27c6ba8ca1eeb34ae
ners <ners@gmx.ch>
2021-03-22 20:12:14 +0100

    Read params safely, default to 1000 bits

--- app/Main.hs
+++ app/Main.hs
@@ -5,11 +5,13 @@ module Main where
 import Lib ( genPrime )
 
 import Data.ByteString.Lazy.UTF8 ( fromString )
+import Data.Maybe ( catMaybes )
 import Data.Text ( unpack )
 import Network.HTTP.Types ( status200 )
 import Network.HTTP.Types.Header ( hContentType )
 import Network.Wai ( responseLBS, pathInfo, Application )
 import Network.Wai.Handler.Warp ( run )
+import Text.Read ( readMaybe )
 
 main :: IO ()
 main = do
@@ -23,4 +25,5 @@ app req f = do
         let body = fromString $ unlines $ show <$> primes
         f $ responseLBS status200 [(hContentType, "text/plain")] body
     where
-        bits = read . unpack <$> pathInfo req
+        parts = let pi = pathInfo req in if null pi then ["1000"] else pi
+        bits = catMaybes $ readMaybe . unpack <$> parts
