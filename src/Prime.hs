bfe8ec8cffc5aaed3b7e064a9acde637e4c07108
ners <ners@gmx.ch>
2021-03-23 10:00:25 +0100

    More elegant prime gen

--- src/Prime.hs
+++ src/Prime.hs
@@ -7,12 +7,11 @@ untilJust :: Monad m => m (Maybe a) -> m a
 untilJust m = go
     where go = m >>= maybe go return
 
+maybePrime :: Integer -> Maybe Integer
+maybePrime n | isPrime n = Just n
+             | otherwise = Nothing
+
 genPrime :: Int -> IO Integer
-genPrime bits = do
-        untilJust $ do
-            n <- randomRIO (min, max)
-            return $ if isPrime n
-               then Just n
-               else Nothing
+genPrime bits = untilJust $ maybePrime <$> randomRIO (min, max)
     where min = 2^bits + 1
           max = 2^(bits + 1) - 1
